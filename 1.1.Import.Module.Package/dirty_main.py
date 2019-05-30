433d0eab3dc1172371f113f645418c3d944ba326
dukov1975 <dukov@icloudpocket.ru>
2019-05-30 14:52:18 +0300

    ADPY-4

--- 1.1.Import.Module.Package/dirty_main.py
+++ 1.1.Import.Module.Package/dirty_main.py
@@ -1,1 +1,10 @@
-from salary import *
+from salary2.people import *
+from salary2.salary import *
+
+def main():
+    for employe in get_employees():
+        calculate_salary(employe)
+
+
+if __name__ == '__main__':
+    main()
