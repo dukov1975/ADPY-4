90992f03d3920f9c9219ef0fb3cfb39d1699577a
dukov1975 <dukov@icloudpocket.ru>
2019-05-30 14:54:14 +0300

    ADPY-4

--- 1.1.Import.Module.Package/dirty_main.py
+++ 1.1.Import.Module.Package/dirty_main.py
@@ -1,8 +1,8 @@
-from salary2.people import *
+from salary2.people import get_employees as all_emp
 from salary2.salary import *
 
 def main():
-    for employe in get_employees():
+    for employe in all_emp():
         calculate_salary(employe)
 
 
