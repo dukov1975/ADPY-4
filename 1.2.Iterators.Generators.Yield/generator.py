4ceb8af842fa956f9b7cdd91df546d9cbc84492f
dukov1975 <dukov@icloudpocket.ru>
2019-06-06 22:28:47 +0300

    ADPY-4

--- 1.2.Iterators.Generators.Yield/generator.py
+++ 1.2.Iterators.Generators.Yield/generator.py
@@ -1,22 +1,11 @@
 import hashlib
 
 
-class Country:
-
-    def __init__(self, file_name ):
-        with open(file_name) as file_data:
-            self.data = []
-            for r_line in file_data.readlines():
-                self.data.append(hashlib.md5(r_line.encode('UTF-8')).hexdigest())
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if not self.data:
-            raise StopIteration
-        return self.data.pop()
+def gen_hash(file_name):
+    with open(file_name) as file_data:
+        for r_line in file_data.readlines():
+            yield hashlib.md5(r_line.encode('UTF-8')).hexdigest()
 
 
-for item in Country('country.urls'):
+for item in gen_hash('country.urls'):
     print(item)
