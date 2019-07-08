029e72ef9d3df994352e28491af172ce12d15e96
dukov1975 <dukov@icloudpocket.ru>
2019-07-08 17:03:48 +0300

    ADPY-4

--- 2.1.Tests/app_test.py
+++ 2.1.Tests/app_test.py
@@ -1,31 +1,45 @@
 import unittest
-import os, json
-from src.app import *
+import json
+import app
+from mock import patch
+
+documents = []
+directories = {}
 
 
-class TestApp(unittest.TestCase):
+def setUpModule():
+    with open('fixtures/documents.json', 'r', encoding='utf-8') as out_docs:
+        documents.extend(json.load(out_docs))
+    with open('fixtures/directories.json', 'r', encoding='utf-8') as out_dirs:
+        directories.update(json.load(out_dirs))
+
+
+@patch('app.documents', documents, create=True)
+@patch('app.directories', directories, create=True)
+class TestSecretaryProgram(unittest.TestCase):
 
     def setUp(self):
-        current_path = str(os.path.dirname(os.path.abspath(__file__)))
-        f_directories = os.path.join(current_path, 'src/fixtures/directories.json')
-        f_documents = os.path.join(current_path, 'src/fixtures/documents.json')
-        with open(f_documents, 'r') as out_docs:
-            self.documents = json.load(out_docs)
-        with open(f_directories, 'r') as out_dirs:
-            self.directories = json.load(out_dirs)
+        self.example_set = {
+            'shelf': 33,
+            'doc': 22
+        }
 
-    def test_insert_document(self):
-        for doc_number in range(1, 4):
-            for doc_type in ['passport', 'invoice', 'insurance']:
-                for doc_owner_name in ['Гонджубасов Кинстинктин', 'Упырев Михаил', 'КонЧенЫй Ли']:
-                    new_doc = {
-                        "type": doc_type,
-                        "number": doc_number,
-                        "name": doc_owner_name
-                    }
-                    self.documents.append(new_doc)
-                    append_doc_to_shelf(doc_number, f'Полка {doc_number}')
+    def test_get_all_doc_owners_names(self):
+        self.assertIsInstance(app.get_all_doc_owners_names(), set)
+        self.assertGreater(len(app.get_all_doc_owners_names()), 0)
+
+    def test_append_doc_to_shelf(self):
+        app.append_doc_to_shelf(self.example_set['doc'], self.example_set['shelf'])
+        self.assertIn(self.example_set['doc'], directories.get(self.example_set['shelf']))
+
+    def test_delete_doc(self):
+        self.assertTrue(app.check_document_existance("11-2"))  # check before deleting doc
+
+        with patch('app.input', return_value="11-2") as child1:
+            app.delete_doc()
+
+        self.assertFalse(app.check_document_existance("11-2"))  # check after deleting doc
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
